b208678523137435fd086f10c845a0825e053720
teobis00 <teobis@MacBook-Pro-de-Teobis.local>
2021-04-26 23:03:52 -0400

    cambio en pagina de proyectos

--- pages/proyecto/_id.vue
+++ pages/proyecto/_id.vue
@@ -5,24 +5,7 @@
       <h1 class="title">
         {{ $route.params.id }}
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <div class="links"></div>
     </div>
   </div>
 </template>
